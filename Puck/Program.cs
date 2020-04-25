8105491507b41c35dd7f273b3971e01712b3877d
Ernest Gu <ernest.gu.3.14@gmail.com>
2020-04-25 12:07:04 -0700

    Added stub project with basic mirroring

---
+++ Puck/Program.cs
@@ -0,0 +1,85 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Puck {
+	class Program {
+		private static DiscordClient discord;
+
+		private const string path_token = @"token.txt";
+		private const int color_embed = 0x45800C;
+
+		static void Main() {
+			const string title_ascii =
+				@"  ______           _    " + "\n" +
+				@"  | ___ \         | |   " + "\n" +
+				@"  | |_/ /   _  ___| | __" + "\n" +
+				@"  |  __/ | | |/ __| |/ /" + "\n" +
+				@"  | |  | |_| | (__|   < " + "\n" +
+				@"  \_|   \__,_|\___|_|\_\" + "\n";
+			Console.WriteLine(title_ascii);
+			MainAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+		}
+
+		static async Task MainAsync() {
+			Console.WriteLine("Starting up...");
+			InitBot();
+
+			discord.MessageCreated += async e => {
+				if (e.Message.Author.Username == discord.CurrentUser.Username) {
+					return; // Never respond to self!
+				}
+
+				bool isMentioned = false;
+				foreach (DiscordUser mention in e.Message.MentionedUsers) {
+					if (mention.IsCurrent) {
+						isMentioned = true;
+						break;
+					}
+				}
+
+				if (isMentioned) {
+					_ = e.Message.Channel.TriggerTypingAsync(); // we don't want to await!
+
+					Console.WriteLine("New message" + e.Message.Content);
+					await e.Message.RespondAsync("Test");
+				}
+			};
+
+			discord.Ready += async e => {
+				DiscordActivity helptext =
+					new DiscordActivity(@"#lfg for pings", ActivityType.Watching);
+				await discord.UpdateStatusAsync(helptext);
+				Console.WriteLine("Startup complete.");
+				Console.WriteLine("Monitoring messages...\n");
+			};
+
+			await discord.ConnectAsync();
+			await Task.Delay(-1);
+		}
+
+		// Init discord client with token from text file.
+		// This allows the token to be separated from source code.
+		static void InitBot() {
+			Console.WriteLine("  Reading auth token...");
+			string bot_token = "";
+			using (StreamReader file = File.OpenText(path_token)) {
+				bot_token = file.ReadLine();
+			}
+			if (bot_token != "")
+				Console.WriteLine("  Auth token found.");
+			else
+				Console.WriteLine("  Auth token missing!");
+
+			discord = new DiscordClient(new DiscordConfiguration {
+				Token = bot_token,
+				TokenType = TokenType.Bot
+			});
+		}
+	}
+}
