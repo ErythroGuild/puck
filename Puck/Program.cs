547ab7c903e09f1671f4a3d94c4dd93294d9f326
Ernest Gu <ernest.gu.3.14@gmail.com>
2020-04-26 06:04:18 -0700

    Added mockup of button (reaction) responses
    only works for group owner for now

--- Puck/Program.cs
+++ Puck/Program.cs
@@ -17,16 +17,24 @@ namespace Puck {
 
 		private const string path_token = @"token.txt";
 		//private const ulong ch_lfg_id = 542093438238326804;
-		private const ulong ch_lfg_id = 489274692255875091;	// #test
-		private const int color_embed = 0x45800C;
+		private const ulong ch_lfg_id = 489274692255875091; // #test
+		private const string
+			emoji_tank_str = ":shield:",
+			emoji_heal_str = ":candle:",
+			emoji_dps_str = ":archery:";
+		private const string
+			emoji_refresh_str = ":arrows_counterclockwise:",
+			emoji_delist_str = ":white_check_mark:";
 
-		private enum GroupType {
-			Other = -1,
-			Dungeon, Raid,
-			Island, Warfront, Vision
+		private static Dictionary<ulong, GroupEntry> table_bulletins = new Dictionary<ulong, GroupEntry>();
+
+		private struct GroupEntry {
+			public DiscordMessage bulletin;
+			public GroupOptions options;
+			public GroupDungeon group;
 		};
 
-		private struct PuckOptions {
+		private struct GroupOptions {
 			public DiscordUser owner;
 			public bool isHelp;
 			public GroupType type;
@@ -34,10 +42,23 @@ namespace Puck {
 			public string title;
 			public DateTimeOffset expiry;
 			public bool isConfig;
-		}
+			public ConfigOptions config;
+		};
 
-		public struct GroupDungeon {
+		private struct GroupDungeon {
 			public int tank, heal, dps;
+		};
+
+		private struct ConfigOptions {
+			DiscordChannel	bulletin_channel;
+			string			default_mention;
+			DateTimeOffset	default_expiry;
+		};
+
+		private enum GroupType {
+			Other = -1,
+			Dungeon, Raid,
+			Island, Warfront, Vision
 		};
 
 		static void Main() {
@@ -73,41 +94,79 @@ namespace Puck {
 					_ = e.Message.Channel.TriggerTypingAsync();	// we don't want to await
 
 					Console.WriteLine("Raw message:\n" + e.Message.Content);
-					PuckOptions command = ParseCommand(e.Message);
-					GroupDungeon group = new GroupDungeon() { tank = 1, heal = 0, dps = 1 };
+					GroupOptions command = ParseCommand(e.Message);
+					GroupDungeon group = new GroupDungeon() { tank = 0, heal = 0, dps = 0 };
 
-					string bulletin = "";
-					if (command.mention != "")
-						bulletin += "@" + command.mention + " ";
-					bulletin += Format.Bold(command.title) + "\n";
-					bulletin += "group lead: " + command.owner.Mention + "\n";
-					bulletin += ToString(group) + "\n";
-					bulletin += Format.Italicize("this group will be delisted in ~");
+					// Construct a bulletin post.
+					string bulletin = ConstructBulletin(command, group);
 
+					// Set up controls on the bulletin and save it to a table.
 					DiscordMessage message_sent = await discord.SendMessageAsync(ch_lfg, bulletin);
-					await message_sent.CreateReactionAsync(emoji_tank);
-					await message_sent.CreateReactionAsync(emoji_heal);
-					await message_sent.CreateReactionAsync(emoji_dps);
-					await message_sent.CreateReactionAsync(emoji_refresh);
-					await message_sent.CreateReactionAsync(emoji_delist);
+					await CreateControls(message_sent);
+					ulong message_id = message_sent.Id;
+					GroupEntry entry = new GroupEntry() {
+						bulletin = message_sent,
+						options = command,
+						group = group,
+					};
+					table_bulletins.Add(message_id, entry);
 				}
 			};
 
 			discord.Ready += async e => {
+				// Initialize channels
 				ch_lfg = await discord.GetChannelAsync(ch_lfg_id);
 
-				emoji_tank = DiscordEmoji.FromName(discord, ":shield:");
-				emoji_heal = DiscordEmoji.FromName(discord, ":candle:");
-				emoji_dps  = DiscordEmoji.FromName(discord, ":bow_and_arrow:");
-				emoji_refresh = DiscordEmoji.FromName(discord, ":arrows_counterclockwise:");
-				emoji_delist  = DiscordEmoji.FromName(discord, ":white_check_mark:");
+				// Initialize emojis
+				emoji_tank = DiscordEmoji.FromName(discord, emoji_tank_str);
+				emoji_heal = DiscordEmoji.FromName(discord, emoji_heal_str);
+				emoji_dps  = DiscordEmoji.FromName(discord, emoji_dps_str);
+				emoji_refresh = DiscordEmoji.FromName(discord, emoji_refresh_str);
+				emoji_delist  = DiscordEmoji.FromName(discord, emoji_delist_str);
 
+				// Set "custom status" (TODO: still waiting for an actual API)
 				DiscordActivity helptext =
 					new DiscordActivity(@"#lfg for pings", ActivityType.Watching);
 				await discord.UpdateStatusAsync(helptext);
 
 				Console.WriteLine("Startup complete.\n");	// extra newline
 				Console.WriteLine("Monitoring messages...\n");
+			};
+
+			discord.MessageReactionAdded += async e => {
+				ulong message_id = e.Message.Id;
+				if (table_bulletins.ContainsKey(message_id)) {
+					GroupEntry entry = table_bulletins[message_id];
+
+					if (e.User == entry.options.owner) {
+						// modulo 1/1/3, but +1 because counting "0" as a state
+						Console.WriteLine("reaction: " + e.Emoji.GetDiscordName());
+						switch (e.Emoji.GetDiscordName()) {
+						case emoji_tank_str:
+							entry.group.tank = (entry.group.tank + 1) % 2;
+							table_bulletins[message_id] = entry;
+							// TODO: Add removal reason for audit logs
+							await entry.bulletin.DeleteReactionAsync(emoji_tank, e.User);
+							break;
+						case emoji_heal_str:
+							entry.group.heal = (entry.group.heal + 1) % 2;
+							table_bulletins[message_id] = entry;
+							// TODO: Add removal reason for audit logs
+							await entry.bulletin.DeleteReactionAsync(emoji_heal, e.User);
+							break;
+						case emoji_dps_str:
+							entry.group.dps = (entry.group.dps + 1) % 4;
+							table_bulletins[message_id] = entry;
+							// TODO: Add removal reason for audit logs
+							await entry.bulletin.DeleteReactionAsync(emoji_dps, e.User);
+							break;
+						default:
+							break;
+						}
+					}
+
+					await UpdateBulletin(entry.bulletin);
+				}
 			};
 
 			await discord.ConnectAsync();
@@ -133,9 +192,9 @@ namespace Puck {
 			});
 		}
 
-		static PuckOptions ParseCommand(DiscordMessage message) {
+		static GroupOptions ParseCommand(DiscordMessage message) {
 			string command = message.Content;
-			PuckOptions options = new PuckOptions() {
+			GroupOptions options = new GroupOptions() {
 				owner = message.Author,
 				isHelp = true,
 				type = GroupType.Dungeon,
@@ -178,6 +237,33 @@ namespace Puck {
 			}
 
 			return output;
+		}
+
+		static async Task CreateControls(DiscordMessage message) {
+			await message.CreateReactionAsync(emoji_tank);
+			await message.CreateReactionAsync(emoji_heal);
+			await message.CreateReactionAsync(emoji_dps);
+			await message.CreateReactionAsync(emoji_refresh);
+			await message.CreateReactionAsync(emoji_delist);
+		}
+
+		static string ConstructBulletin(GroupOptions command, GroupDungeon group) {
+			string bulletin = "";
+			if (command.mention != "")
+				bulletin += "@" + command.mention + " ";
+			bulletin += Format.Bold(command.title) + "\n";
+			bulletin += "group lead: " + command.owner.Mention + "\n";
+			bulletin += ToString(group) + "\n";
+			bulletin += Format.Italicize("this group will be delisted in ~");
+			return bulletin;
+		}
+
+		static async Task UpdateBulletin(DiscordMessage message) {
+			string bulletin = ConstructBulletin(
+				table_bulletins[message.Id].options,
+				table_bulletins[message.Id].group
+			);
+			await message.ModifyAsync(bulletin);
 		}
 	}
 }
