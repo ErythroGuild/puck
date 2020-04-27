a4085ead6217991023afd7c1c85dc81a551cf463
Ernest Gu <ernest.gu.3.14@gmail.com>
2020-04-27 16:47:11 -0700

    Added Bulletin class
    no ctor yet

---
+++ Puck/Bulletin.cs
@@ -0,0 +1,33 @@
+﻿using DSharpPlus.Entities;
+
+using System;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Puck {
+	class Bulletin {
+		public DiscordMessage message;
+		public Timer updater;
+		public BulletinData data;
+
+		public event EventHandler<ulong> Delisted;
+
+		public async Task Update() {
+			string bulletin_new = data.ToString();
+			await message.ModifyAsync(bulletin_new);
+
+			// TODO: add a warning 1:30 before delisting?
+			if (data.expiry < DateTimeOffset.Now) {
+				string notification = "";
+				notification +=
+					"Your group " +
+					Format.Bold(data.title) +
+					" has been delisted. :white_check_mark:";
+				_ = data.owner.SendMessageAsync(notification);  // no need to await
+				// TODO: move notification to Puck.Program?
+
+				Delisted(this, message.Id);
+			}
+		}
+	}
+}
