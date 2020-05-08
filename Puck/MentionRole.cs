db165eba00e250d6f2c24364afb37a18d1c1fed0
Ernest Gu <ernest.gu.3.14@gmail.com>
2020-05-07 17:24:48 -0700

    Added a MentionRole class
    Since the `.EveryoneRole` isn't a real role... (see #21)

---
+++ Puck/MentionRole.cs
@@ -0,0 +1,63 @@
+﻿using DSharpPlus.Entities;
+
+namespace Puck {
+	class MentionRole {
+		public enum Type {
+			None = 0,
+			Here, Everyone,
+			Discord,
+		};
+
+		Type type;
+		DiscordRole? discord_role;
+
+		public MentionRole(Type type, DiscordRole? role = null) {
+			this.type = type;
+			if (type == Type.Discord) {
+				discord_role = role;
+			} else {
+				discord_role = null;
+			}
+		}
+		public MentionRole(DiscordRole role) {
+			type = Type.Discord;
+			discord_role = role;
+		}
+
+		public void Set(Type type, DiscordRole? role = null) {
+			this.type = type;
+			if (type == Type.Discord) {
+				discord_role = role;
+			}
+		}
+
+		public Type RoleType() {
+			return type;
+		}
+		public DiscordRole? GetDiscordRole() {
+			return discord_role;
+		}
+
+		public string? Mention() {
+			// default case will only happen if type was casted
+			return type switch {
+				Type.None		=> null,
+				Type.Here		=> "@here",
+				Type.Everyone	=> "@everyone",
+				Type.Discord	=> discord_role!.Mention,
+				_ => null,
+			};
+		}
+
+		public string Name() {
+			// default case will only happen if type was casted
+			return type switch {
+				Type.None		=> "none",
+				Type.Here		=> "here",
+				Type.Everyone	=> "everyone",
+				Type.Discord	=> discord_role!.Name,
+				_ => "none",
+			};
+		}
+	}
+}
