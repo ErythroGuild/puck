c55a481d8cb9e6444c52d45b503a58d550aeb4cf
Ernest Gu <ernest.gu.3.14@gmail.com>
2020-05-08 08:13:46 -0700

    Added (fully commented!) Emoji wrapper class
    For #39, but also helps with #14.

---
+++ Puck/Emoji.cs
@@ -0,0 +1,71 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+
+using System;
+using System.Collections.Generic;
+
+namespace Puck {
+	static class Emoji {
+		public enum Type {
+			Tank, Heal, Dps,
+			Refresh, Delist,
+		};
+
+		static Dictionary<Type, DiscordEmoji> type_to_emoji =
+			new Dictionary<Type, DiscordEmoji>();
+		// Default emojis if custom emojis could not be found.
+		// Tank:	Shield
+		// Heal:	Flag: Switzerland
+		// Dps:		Bow and Arrow
+		// Refresh:	Counterclockwise Arrows Button
+		// Delist:	Check Mark Button
+		static Dictionary<Type, string> type_to_unicode =
+			new Dictionary<Type, string>() {
+				{ Type.Tank,	"\uD83D\uDEE1" },
+				{ Type.Heal,	"\uD83C\uDDE8\uD83C\uDDED" },
+				{ Type.Dps,		"\uD83C\uDFF9" },
+				{ Type.Refresh,	"\uD83D\uDD04" },
+				{ Type.Delist,	"\u2705" },
+			};
+		// Custom emojis (requires a connection to Erythro server).
+		static Dictionary<Type, string> type_to_custom =
+			new Dictionary<Type, string> {
+				{ Type.Tank,	":r_tank:" },
+				{ Type.Heal,	":r_heal:" },
+				{ Type.Dps,		":r_dps:" },
+			};
+
+		// Various different ways to fetch a DiscordEmoji.
+		public static DiscordEmoji Get(this Type type) => From(type);
+		public static DiscordEmoji From(Type type) {
+			return type_to_emoji[type];
+		}
+		public static DiscordEmoji From(Group.Role role) {
+			return role switch {
+				Group.Role.Tank => From(Type.Tank),
+				Group.Role.Heal => From(Type.Heal),
+				Group.Role.Dps  => From(Type.Dps),
+				_ => throw new ArgumentException(),
+			};
+		}
+
+		// Custom emoji initialization (requires a connected client).
+		public static void Init(DiscordClient client) {
+			foreach (Type type in type_to_custom.Keys) {
+				DiscordEmoji emoji =
+					DiscordEmoji.FromName(client, type_to_custom[type]);
+				type_to_emoji[type] = emoji;
+			}
+		}
+
+		// Static initializer to populate emojis with default ones.
+		// (From unicode since guild emojis require a connected client.)
+		static Emoji() {
+			foreach (Type type in type_to_unicode.Keys) {
+				string unicode = type_to_unicode[type];
+				DiscordEmoji emoji = DiscordEmoji.FromUnicode(unicode);
+				type_to_emoji.Add(type, emoji);
+			}
+		}
+	}
+}
