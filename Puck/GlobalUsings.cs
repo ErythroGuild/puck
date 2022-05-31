cec4b79c12903f081693339f0a81302988fadf51
Ernest Gu <ernest.gu.3.14@gmail.com>
2022-05-31 14:19:03 -0700

    Add a global usings file

---
+++ Puck/GlobalUsings.cs
@@ -0,0 +1,23 @@
+﻿// System.
+global using System;
+global using System.Collections.Concurrent;
+global using System.Collections.Generic;
+global using System.Collections.ObjectModel;
+global using System.Diagnostics;
+global using System.IO;
+global using System.Threading.Tasks;
+
+// D#+.
+global using DSharpPlus;
+global using DSharpPlus.Entities;
+global using DSharpPlus.EventArgs;
+global using DSharpPlus.Exceptions;
+
+// Microsoft.
+global using Microsoft.Extensions.Logging;
+
+// Serilog.
+global using Serilog;
+
+// Project namespaces.
+global using Puck.Utils;
