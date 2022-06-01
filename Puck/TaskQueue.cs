629f3631b76080a4273d86210abe619b5a17f642
Ernest Gu <ernest.gu.3.14@gmail.com>
2022-06-01 14:10:31 -0700

    Add TaskQueue class

---
+++ Puck/TaskQueue.cs
@@ -0,0 +1,28 @@
+﻿namespace Puck;
+
+class TaskQueue {
+	public bool IsRunning => _task.IsCompleted;
+
+	private readonly ConcurrentQueue<Task> _queue = new ();
+	private Task _task = Task.CompletedTask;
+
+	public void Add(Task action) {
+		_queue.Enqueue(action);
+
+		if (!IsRunning) {
+			_task = Task.Run(async () => {
+				while (!_queue.IsEmpty) {
+					_queue.TryDequeue(out Task? task);
+
+					if (task is null)
+						continue;
+
+					if (task.Status == TaskStatus.Created)
+						task.Start();
+
+					await task;
+				}
+			});
+		}
+	}
+}
