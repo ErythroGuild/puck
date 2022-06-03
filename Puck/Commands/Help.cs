9c29b28ecc99abf1e6eac7cc02494b78903caa5f
Ernest Gu <ernest.gu.3.14@gmail.com>
2022-06-03 03:44:28 -0700

    Finish fleshing out `/help` to be guild-specific

--- Puck/Commands/Help.cs
+++ Puck/Commands/Help.cs
@@ -1,26 +1,29 @@
-using static Puck.Commands.CommandHandler.CommandTree;
+﻿using static Puck.Commands.CommandHandler.CommandTree;
 
 namespace Puck.Commands;
 
 class Help : CommandHandler {
 	public override CommandTree Tree { get; init; }
 
+	private const int _capRoles = 8;
+
 	private readonly Emojis _emojis;
+	private const string
+		_commandHelp = "help";
 	private const string
 		_zwsp = "\u200B",
 		_ensp = "\u2002",
 		_emsp = "\u2003",
+		_ellp = "\u22EF",
 		_bbul = "\u2022",
 		_wbul = "\u25E6",
 		_arrr = "\u2192";
-	private const string
-		_commandHelp = "help";
 
 	public Help(Emojis emojis) {
 		Tree =  new (
 			new (new LeafArgs(
 				_commandHelp,
-				"Show how to use Puck's commands.",
+				"Explain how to use Puck's commands.",
 				new List<CommandOption>(),
 				Permissions.None
 			), HelpAsync)
@@ -29,7 +32,15 @@ class Help : CommandHandler {
 		_emojis = emojis;
 	}
 
-	private Task HelpAsync(DiscordInteraction interaction, Dictionary<string, object> _) {
+	private async Task HelpAsync(DiscordInteraction interaction, Dictionary<string, object> _) {
+		await interaction.CreateResponseAsync(
+			InteractionResponseType.DeferredChannelMessageWithSource,
+			new DiscordInteractionResponseBuilder()
+				.AsEphemeral(true)
+		);
+
+		DiscordGuild guild = interaction.Guild;
+
 		string helptext =
 			$"""
 			**Sign Up**
@@ -40,10 +51,10 @@ class Help : CommandHandler {
 			**List Group**
 			**`/lfg`** creates a new thread for your group.
 			`title` is the only mandatory field (all others can be omitted).
-			`mention` only works with these roles:
-			{_emsp}{_wbul}{_ensp}@role
-			`group-type` is "5-man (1-1-3)" by default. "Raid group" has a cap of 40 members.
-			`duration` is 5 minutes by default. The thread will be archived after this time.
+			{GetMentionHelp(guild)}
+			`group-type` depends on `mention` by default. ("Raid group" caps at {Group.MaxMembers} members.)
+			{GetGroupTypeHelp(guild)}
+			`duration` is `{GetDefaultDuration(guild)}` by default. The thread will be archived after this time.
 
 			**Set Up Group**
 			Use the signup buttons ({_emojis.Tank}/{_emojis.Heal}/{_emojis.Dps}) to pre-fill spots in your group.
@@ -54,12 +65,109 @@ class Help : CommandHandler {
 			*Note: use Server Settings {_arrr} Integrations to set up allowed channels.*
 			`/config help` displays a guide with further details.
 			""";
-		return interaction.CreateResponseAsync(
-			InteractionResponseType.ChannelMessageWithSource,
-			new DiscordInteractionResponseBuilder()
+		await interaction.EditOriginalResponseAsync(
+			new DiscordWebhookBuilder()
 				.WithContent(helptext)
 				.AddMentions(Mentions.None)
-				.AsEphemeral(true)
 		);
+	}
+
+	private static string GetMentionHelp(DiscordGuild guild) {
+		IReadOnlyDictionary<ulong, DiscordRole> roleTable =
+			guild.Roles;
+
+		using GuildConfigDatabase database = new ();
+		GuildConfig config = database.GetConfig(guild.Id)
+			?? new (guild);
+
+		List<string> roles = new ()
+			{"`mention` only works with these roles:"};
+		foreach (ulong roleId in config.RoleList()) {
+			if (roleTable.ContainsKey(roleId))
+				roles.Add($"{_emsp}{_wbul}{_ensp}{roleTable[roleId].Mention}");
+		}
+
+		// Special case if no roles are enabled.
+		if (roles.Count == 0) {
+			roles = new () {
+				"`mention` is *not enabled* for any roles.",
+				$"{_emsp}{_ensp}To change this setting for this server, use `/config mention`.",
+				$"{_emsp}{_ensp}(You will need \"Manage Server\" permissions.)",
+			};
+		}
+
+		// Cap list to a reasonable number (+1 for the first line).
+		if (roles.Count > 1 + _capRoles) {
+			// Subtracting an additional two lines means when a list
+			// hits the cap, 2 extra lines will be elided, ensuring
+			// a minimum number of elided lines.
+			// (This prevents "orphaning" a single line.)
+			roles = roles.GetRange(0, 1 + _capRoles - 2);
+			roles.Add($"{_emsp}{_ensp}{_ensp}{_ellp}{_emsp}*(see `/config mention list` for full list)*");
+		}
+
+		return roles.ToLines();
+	}
+
+	private static string GetGroupTypeHelp(DiscordGuild guild) {
+		IReadOnlyDictionary<ulong, DiscordRole> roleTable =
+			guild.Roles;
+
+		using GuildConfigDatabase database = new ();
+		GuildConfig config = database.GetConfig(guild.Id)
+			?? new (guild);
+
+		// Show default group type first.
+		List<string> roles = new ();
+		string? type_default = LFG.GroupTypeName(config.DefaultGroupType);
+		if (type_default is not null)
+			roles.Add($"{_emsp}{_wbul}{_ensp}`{type_default,-13}` if no `mention` specified");
+
+		// Compile all mentions.
+		IReadOnlyDictionary<ulong, string> typeTable =
+			config.RoleGroupTypes();
+		foreach (ulong roleId in typeTable.Keys) {
+			if (roleTable.ContainsKey(roleId)) {
+				string? type = LFG.GroupTypeName(typeTable[roleId]);
+				DiscordRole role = roleTable[roleId];
+				if (type is not null)
+					roles.Add($"{_emsp}{_wbul}{_ensp}`{type,-13}` for {role.Mention}");
+			}
+		}
+
+		// Cap list to a reasonable number (+1 for the first line).
+		if (roles.Count > 1 + _capRoles) {
+			// Subtracting an additional two lines means when a list
+			// hits the cap, 2 extra lines will be elided, ensuring
+			// a minimum number of elided lines.
+			// (This prevents "orphaning" a single line.)
+			roles = roles.GetRange(0, 1 + _capRoles - 2);
+			roles.Add($"{_emsp}{_ensp}{_ensp}{_ellp}{_emsp}*(see `/config mention list` for full list)*");
+		}
+
+		return roles.ToLines();
+	}
+
+	private static string GetDefaultDuration(DiscordGuild guild) {
+		using GuildConfigDatabase database = new ();
+		GuildConfig config = database.GetConfig(guild.Id)
+			?? new (guild);
+
+		TimeSpan duration = config.DefaultDuration();
+
+		return duration switch {
+			TimeSpan t when t < TimeSpan.FromHours(1) =>
+				duration.ToString("m 'min'"),
+			TimeSpan t when t < TimeSpan.FromHours(4) =>
+				duration.ToString("h 'hrs' m 'min'"),
+			TimeSpan t when t < TimeSpan.FromDays(1) =>
+				duration.ToString("h 'hrs'"),
+			TimeSpan t when t < TimeSpan.FromDays(2) =>
+				duration.ToString("d 'day' h 'hrs'"),
+			TimeSpan t when t < TimeSpan.FromDays(4) =>
+				duration.ToString("d 'days' h 'hrs'"),
+			_ =>
+				duration.ToString("d 'days'"),
+		};
 	}
 }
