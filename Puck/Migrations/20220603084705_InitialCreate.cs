676f0fcad2303b13d2ec4c0223058ca0bf734abb
Ernest Gu <ernest.gu.3.14@gmail.com>
2022-06-03 01:51:58 -0700

    Check in database migrations (might as well...)

---
+++ Puck/Migrations/20220603084705_InitialCreate.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Puck.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GuildConfigs",
+                columns: table => new
+                {
+                    GuildId = table.Column<string>(type: "TEXT", nullable: false),
+                    GuildName = table.Column<string>(type: "TEXT", nullable: false),
+                    DefaultDurationMsec = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GuildConfigs", x => x.GuildId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GroupTypes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GuildId = table.Column<string>(type: "TEXT", nullable: false),
+                    GroupTypeName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GroupTypes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GroupTypes_GuildConfigs_GuildId",
+                        column: x => x.GuildId,
+                        principalTable: "GuildConfigs",
+                        principalColumn: "GuildId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GuildRoles",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GuildId = table.Column<string>(type: "TEXT", nullable: false),
+                    RoleId = table.Column<string>(type: "TEXT", nullable: false),
+                    GroupTypeName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GuildRoles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GuildRoles_GuildConfigs_GuildId",
+                        column: x => x.GuildId,
+                        principalTable: "GuildConfigs",
+                        principalColumn: "GuildId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupTypes_GuildId",
+                table: "GroupTypes",
+                column: "GuildId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GuildRoles_GuildId",
+                table: "GuildRoles",
+                column: "GuildId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GroupTypes");
+
+            migrationBuilder.DropTable(
+                name: "GuildRoles");
+
+            migrationBuilder.DropTable(
+                name: "GuildConfigs");
+        }
+    }
+}
