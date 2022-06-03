2ccea498b333e5394fa4f7874262187a7eeafebd
Ernest Gu <ernest.gu.3.14@gmail.com>
2022-06-03 01:36:34 -0700

    Add convenience functions to GuildConfig database

--- Puck/Database/GuildConfig.cs
+++ Puck/Database/GuildConfig.cs
@@ -1,11 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 
 using Microsoft.EntityFrameworkCore;
 
 namespace Puck.Database;
 
 class GuildConfigDatabase : DbContext {
+	public DbSet<GuildConfig> Configs { get; set; } = null!;
+	
 	private const string _pathConfig =
 		@"Data Source=data/config.sqlite";
 
@@ -19,6 +22,16 @@ class GuildConfigDatabase : DbContext {
 			.HasIndex(t => t.GuildId);
 		modelBuilder.Entity<GroupType>()
 			.HasIndex(t => t.GuildId);
+	}
+
+	public GuildConfig? GetConfig(ulong guildId) {
+		IEnumerable<GuildConfig> config_list =
+			from _config in Configs
+			where _config.GuildId == guildId.ToString()
+			select _config;
+		return config_list.Any()
+			? config_list.First()
+			: null;
 	}
 }
 
@@ -36,6 +49,8 @@ class GuildConfig {
 	[ForeignKey("GuildId")] public virtual ICollection<GuildRole> AllowedRoles      { get; set; }
 	[ForeignKey("GuildId")] public virtual ICollection<GroupType> AllowedGroupTypes { get; set; }
 
+	public GuildConfig(ulong guildId, string guildName)
+		: this (guildId.ToString(), guildName) { }
 	public GuildConfig(string guildId, string guildName) {
 		GuildId = guildId;
 		GuildName = guildName;
@@ -44,6 +59,23 @@ class GuildConfig {
 
 		AllowedRoles = new HashSet<GuildRole>();
 		AllowedGroupTypes = new HashSet<GroupType>();
+	}
+
+	public TimeSpan DefaultDuration() =>
+		TimeSpan.FromMilliseconds(DefaultDurationMsec);
+
+	public IList<ulong> RoleList() {
+		List<ulong> list = new ();
+		foreach (GuildRole role in AllowedRoles)
+			list.Add(ulong.Parse(role.RoleId));
+		return list;
+	}
+
+	public IList<string> GroupTypeList() {
+		List<string> list = new ();
+		foreach (GroupType groupType in AllowedGroupTypes)
+			list.Add(groupType.GroupTypeName);
+		return list;
 	}
 }
 
@@ -54,6 +86,8 @@ class GuildRole {
 	[Required] public string GuildId { get; set; }
 	[Required] public string RoleId  { get; set; }
 
+	public GuildRole(ulong guildId, ulong roleId)
+		: this (guildId.ToString(), roleId.ToString()) { }
 	public GuildRole(string guildId, string roleId) {
 		GuildId = guildId;
 		RoleId = roleId;
@@ -67,6 +101,8 @@ class GroupType {
 	[Required] public string GuildId       { get; set; }
 	[Required] public string GroupTypeName { get; set; }
 
+	public GroupType(ulong guildId, string groupTypeName)
+		: this (guildId.ToString(), groupTypeName) { }
 	public GroupType(string guildId, string groupTypeName) {
 		GuildId = guildId;
 		GroupTypeName = groupTypeName;
