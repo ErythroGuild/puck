e3a42e7bc7504dc89214619e9a436ab43bba3cee
Ernest Gu <ernest.gu.3.14@gmail.com>
2022-06-04 23:55:49 -0700

    Add bulletin database (& database class)

---
+++ Puck/Databases/Bulletin.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Puck.Databases;
+
+class BulletinDatabase : DbContext {
+	public DbSet<Bulletin> Bulletins { get; set; } = null!;
+
+	private const string _pathBulletins =
+		@"Data Source=data/bulletins.sqlite";
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+		optionsBuilder.UseSqlite(_pathBulletins);
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder) {
+		modelBuilder.Entity<Bulletin>();
+	}
+
+	public Bulletin? GetBulletinFromMessage(ulong messageId) {
+		IEnumerable<Bulletin> bulletin_list =
+			from _bulletin in Bulletins
+			where _bulletin.MessageId == messageId.ToString()
+			select _bulletin;
+		return bulletin_list.Any()
+			? bulletin_list.First()
+			: null;
+	}
+
+	public List<Bulletin> GetBulletins() {
+		List<Bulletin> bulletins = new (
+			from _bulletin in Bulletins
+			select _bulletin
+		);
+		return bulletins;
+	}
+}
+
+[Table("Bulletins")]
+class Bulletin {
+	[Key] public string MessageId { get; set; }
+
+	// Bulletin data.
+	[Required] public string GuildId     { get; set; } = null!;
+	[Required] public string ThreadId    { get; set; } = null!;
+	[Required] public string Title       { get; set; } = null!;
+	[Required] public string Description { get; set; } = null!;
+	[Required] public string MentionId   { get; set; } = null!;
+	[Required] public string Expiry      { get; set; } = null!;
+
+	// Group data.
+	[Required] public string OwnerId { get; set; } = null!;
+	[Required] public string TankIds { get; set; } = null!;
+	[Required] public string HealIds { get; set; } = null!;
+	[Required] public string DpsIds  { get; set; } = null!;
+	[Required] public bool AcceptAnyRole { get; set; }
+	[Required] public bool HasMaxCount   { get; set; }
+	[Required] public int TankMax { get; set; }
+	[Required] public int HealMax { get; set; }
+	[Required] public int DpsMax  { get; set; }
+
+	public Bulletin(string messageId) {
+		MessageId = messageId;
+	}
+}
---
+++ Puck/Migrations/BulletinDatabaseMigrations/20220605052840_InitialCreate.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Puck.Migrations.BulletinDatabaseMigrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bulletins",
+                columns: table => new
+                {
+                    MessageId = table.Column<string>(type: "TEXT", nullable: false),
+                    GuildId = table.Column<string>(type: "TEXT", nullable: false),
+                    ThreadId = table.Column<string>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    MentionId = table.Column<string>(type: "TEXT", nullable: false),
+                    Expiry = table.Column<string>(type: "TEXT", nullable: false),
+                    OwnerId = table.Column<string>(type: "TEXT", nullable: false),
+                    TankIds = table.Column<string>(type: "TEXT", nullable: false),
+                    HealIds = table.Column<string>(type: "TEXT", nullable: false),
+                    DpsIds = table.Column<string>(type: "TEXT", nullable: false),
+                    AcceptAnyRole = table.Column<bool>(type: "INTEGER", nullable: false),
+                    HasMaxCount = table.Column<bool>(type: "INTEGER", nullable: false),
+                    TankMax = table.Column<int>(type: "INTEGER", nullable: false),
+                    HealMax = table.Column<int>(type: "INTEGER", nullable: false),
+                    DpsMax = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bulletins", x => x.MessageId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bulletins");
+        }
+    }
+}
