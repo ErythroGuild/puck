d0f8680802c8cf8db21460bd7e9170da5c6e7af5
Ernest Gu <ernest.gu.3.14@gmail.com>
2022-06-10 22:41:02 -0700

    Add a ConstBiMap class for lookup tables

---
+++ Puck/ConstBiMap.cs
@@ -0,0 +1,31 @@
+﻿namespace Puck;
+
+class ConstBiMap<T, U>
+	where T: notnull
+	where U: notnull
+{
+	private readonly ReadOnlyDictionary<T, U> _tableForward;
+	private readonly ReadOnlyDictionary<U, T> _tableReverse;
+
+	public ConstBiMap(IReadOnlyDictionary<T, U> table) {
+		ConcurrentDictionary<T, U> tableForward = new (table);
+		ConcurrentDictionary<U, T> tableReverse = new ();
+
+		foreach (T key in tableForward.Keys) {
+			U value = tableForward[key];
+			if (tableReverse.ContainsKey(value))
+				throw new ArgumentException("Input map is not one-to-one.", nameof(table));
+			else
+				tableReverse.TryAdd(value, key);
+		}
+
+		_tableForward = new (tableForward);
+		_tableReverse = new (tableReverse);
+	}
+
+	public U this[T key] => _tableForward[key];
+	public T this[U key] => _tableReverse[key];
+
+	public U GetForward(T key) => _tableForward[key];
+	public T GetReverse(U key) => _tableReverse[key];
+}
